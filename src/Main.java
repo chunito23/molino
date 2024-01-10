dd2e1d4e9901793758ea74f0d5cad0a1440b3cb9
dominguez pablo maximiliano <dominguez2020@outlook.com.ar>
2024-01-10 15:50:53 -0300

    tatoPelado

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Modelo.*;
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("prueba");
+            System.out.println("tato pelado");
             Jugador j1 = new Jugador();
             Jugador j2 = new Jugador();
             Modelo m = new Modelo(j1,j2);
